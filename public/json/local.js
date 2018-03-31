04ec64fc8042fc766a3776f35c0b2fe1895c64a3
root <root@localhost.localdomain>
2018-03-31 11:11:17 +0800

    first commit

---
+++ public/json/local.js
@@ -0,0 +1,39 @@
+var localdata = [{
+  userName: "胖哥",
+  imageUrl: "./public/image/bb.jpg"
+}, {
+  userName: "DB",
+  imageUrl: "./public/image/aa.jpg"
+  }, {
+  userName: "陈冠希",
+  imageUrl: "./public/image/cc.jpg"
+  }, {
+  userName: "白熏",
+  imageUrl: "./public/image/dd.jpg"
+  }, {
+  userName: "女仆",
+  imageUrl: "./public/image/ff.jpg"
+  }, {
+  userName: "谢霆锋",
+  imageUrl: "./public/image/qq.jpg"
+    }, {
+  userName: "陈小春",
+  imageUrl: "./public/image/ww.jpg"
+    }, {
+  userName: "吴建豪",
+  imageUrl: "./public/image/ee.jpg"
+    }, {
+  userName: "张震岳",
+  imageUrl: "./public/image/rr.jpg"
+    }, {
+  userName: "泰迪",
+  imageUrl: "./public/image/tt.jpg"
+    }, {
+  userName: "吴彦祖",
+  imageUrl: "./public/image/yy.jpg"
+    }, {
+  userName: "GAI",
+  imageUrl: "./public/image/uu.jpg"
+}]
+
+
